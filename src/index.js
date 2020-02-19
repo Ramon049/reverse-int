5653533a5399afde65685fc2dc3f02eae2155dd5
Roman Koshla <koshla.roman10@gmail.com>
2020-02-19 23:16:30 +0400

    feat: add solve for reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+    let res;
+    
+    if(n >= 0){
+        res = n.toString().split('').reverse().join('');
+    }
+    if(n < 0){
+        n *= -1; 
+        res = n.toString().split('').reverse().join('');
+       
+    }
+  return +res;
 }
